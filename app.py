6d8115be185d3085e5236d38edd359228ba15abd
ledunuri <lasyaedunuri@gmail.com>
2024-04-01 19:11:14 -0400

    updated

--- app.py
+++ app.py
@@ -19,12 +19,13 @@ selected_y_var = st.selectbox(
 
 penguin_file = st.file_uploader("Select Your Local Penguins CSV")
 
+@st.cache.data()
 def load_file(penguin_file):
     time.sleep(5)
     if penguin_file is not None:
         penguins_df = pd.read_csv(penguin_file)
     else:
-        penguins_df = pd.read_csv('penguin.csv')
+        penguins_df = pd.read_csv('penguins.csv')
         return(penguins_df)
     
 penguins_df = load_file(penguin_file)
